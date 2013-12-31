0d9985df1ffba4dbf5c001ccb3a0621a115e4f37
roygoldman <firelord665@gmail.com>
2013-12-31 18:15:17 -0500

    made some changes to pom.xml

--- src/com/richgerdes/bitcointrader/MarketStreamer.java
+++ src/com/richgerdes/bitcointrader/MarketStreamer.java
@@ -5,9 +5,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.slf4j.ILoggerFactory;
-import org.slf4j.LoggerFactory;
-
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.ExchangeFactory;
 import com.xeiam.xchange.dto.marketdata.OrderBookUpdate;
